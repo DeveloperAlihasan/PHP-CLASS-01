7a893239f7d32e0c454211968215c8b49d29b21d
DeveloperAlihasan <mhasan202576@gmail.com>
2022-03-24 19:57:35 +0600

    24-03-2022

---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- /*
+let's introduce proudct,
+me: i have product for sell.
+you: what kind of product?
+me: i have hp monitor.
+you: is it old monitor ?
+me: yes,this is old monitor.
+you : quantity of your product ?
+me: three product,15.6" display.
+you: do you give VGA cable?
+me: yes.
+you: how much do you want for those products?
+me: 8000 taka .
+*/ -->
+
+
+
+<?php  
+
+$product = "hp monitor" ;  // datatype is string
+
+$product_quantity = 3 ;  // datatype is integer
+
+$product_size = 15.6 ; // datatype is  float
+
+$product_is_old = true ; // datatype is  boolean
+
+$product_price = array("8000","8500" ,"9000"); // datatype is  array
+
+$vga_cable = null ;  // datatype is  null
+
+
+var_dump ($product);
+var_dump ($product_quantity);
+var_dump ($product_size);
+var_dump ($product_is_old);
+var_dump ($product_price);
+var_dump ($vga_cable);
+
+ ?>
+
+ 
+    
+
